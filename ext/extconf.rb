ed7959b8a9b2a29a5f12cb77b65d5802c57daa1a
Genta Kaneyama <genta-kaneyama@cookpad.com>
2012-06-20 16:02:13 +0900

    fix extconf.rb for Mac OS env

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,10 @@
 require 'mkmf'
-CONFIG["LDSHARED"] = "g++ -shared"
+CONFIG["LDSHARED"] = case RUBY_PLATFORM
+                     when /darwin/
+                       "g++ -dynamic -bundle -undefined suppress -flat_namespace"
+                     else
+                       "g++ -shared"
+                     end
 $CFLAGS = "#{ENV['CFLAGS']} -Wall -O3 "
 if CONFIG["MAJOR"].to_i >= 1 && CONFIG["MINOR"].to_i >= 8
   $CFLAGS << " -DHAVE_DEFINE_ALLOC_FUNCTION"
